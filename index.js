1d970e1749048659dba9310974d0d0716ad85072
Glenn Piludu <gpiludu@gmail.com>
2022-05-22 11:26:57 -0500

    Added new homepage; fixed content height on eucalyptus

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+import 'dotenv/config'
 import express from 'express'
 import path from 'path'
+import livereload from 'livereload'
+import connectLiveReload from 'connect-livereload'
 
 const app = express()
 const __dirname = process.cwd()
+
+// Live reload server for development
+if (process.env.NODE_ENV !== 'production') {
+  const liveReloadServer = livereload.createServer()
+  liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+      liveReloadServer.refresh("/")
+    }, 50)
+  })
+
+  app.use(connectLiveReload())
+}
+
 
 app.set({
   'Content-Type': 'text/html'
@@ -10,10 +26,16 @@ app.set({
 
 app.use(express.static(__dirname + '/public'))
 
+// Page routes
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'html', 'index.html'))
 })
 
+app.get('/eucalyptus', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'html', 'eucalyptus.html'))
+})
+
+// Start the server
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
