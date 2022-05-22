3b6cac6c5d8b26f4609b83912d81e38ac582dd0d
Glenn Piludu <gpiludu@gmail.com>
2022-05-22 11:36:01 -0500

    moved livereload imports

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import 'dotenv/config'
 import express from 'express'
 import path from 'path'
-import livereload from 'livereload'
-import connectLiveReload from 'connect-livereload'
+
 
 const app = express()
 const __dirname = process.cwd()
 
 // Live reload server for development
+import livereload from 'livereload'
+import connectLiveReload from 'connect-livereload'
+
 if (process.env.NODE_ENV !== 'production') {
   const liveReloadServer = livereload.createServer()
   liveReloadServer.server.once("connection", () => {
